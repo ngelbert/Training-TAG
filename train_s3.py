bd031d0f8e0e9901464b5646b1cf95126a86b571
Elbert Ng <ng.elbert@yahoo.com>
2021-04-06 22:34:24 -0700

    decision tree final fix

--- train_s3.py
+++ train_s3.py
@@ -18,11 +18,8 @@ ds = pd.read_csv("tree_dataset/behavior.csv", header=None, names=all_col)
 # Process label encoder to change categorical feature to numerical feature
 LabEnc = preprocessing.LabelEncoder()
 LabEnc.fit(ds['Behavior'])
-X= ds[features]
-Y= ds['Behavior']
-
-# 80/20 train test split
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+X_train = ds[features]
+Y_train = ds['Behavior']
 
 # Train the model with built in decision tree algorithm in sklearn
 clf = DecisionTreeClassifier(criterion="entropy")
